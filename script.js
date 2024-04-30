ff2397ecd5ebdedec13cd9df3d3824d46bd81eb2
Martin Aguilar <112834005+TreDogPlay@users.noreply.github.com>
2024-04-29 20:45:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 let menuVisible = false;
-//Función que oculta o muestra el menu
+//Funcion que oculta o muestra el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
-        document.getElementById("nav").classList ="";
+        document.getElementById("nav").classList="";
         menuVisible = false;
     }else{
-        document.getElementById("nav").classList ="responsive";
+        document.getElementById("nav").classList="responsive";
         menuVisible = true;
     }
 }
 
 function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
+    //ocultar el menu una vez que se selecciono una opcion
+    document.getElementById("nav").classList="";
+    menuVisible=false;
 }
+
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[0].classList.add("autoCAD");
+        habilidades[1].classList.add("gestionEmpresarial");
+        habilidades[2].classList.add("gestionProyectos");
+        habilidades[3].classList.add("habilidadesdibujo");
+        habilidades[4].classList.add("adaptabilidad");
+        habilidades[5].classList.add("renderizado");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("equipo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("critico");
     }
 }
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
+//detecto el scrolling para aplicar la animacion de la barra de animaciones
 window.onscroll = function(){
     efectoHabilidades();
-} 
+}
+
+//Funcion descargar CV
+document.getElementById("descargarCV").addEventListener("click", function() {
+    var enlaceDescarga = document.createElement('a');
+    enlaceDescarga.href = 'Document/prueba.pdf'; // Ruta al archivo PDF corregida
+    enlaceDescarga.download = 'prueba.pdf'; // Nombre del archivo PDF
+    document.body.appendChild(enlaceDescarga);
+    enlaceDescarga.click();
+    document.body.removeChild(enlaceDescarga);
+});
