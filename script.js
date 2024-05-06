290cce877ac752bf8b83b2d7f305f4b9337d8e1f
Martin Aguilar <112834005+TreDogPlay@users.noreply.github.com>
2024-05-06 12:40:18 -0600

    CV Martin Aguilar

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("autoCAD");
-        habilidades[1].classList.add("gestionEmpresarial");
-        habilidades[2].classList.add("gestionProyectos");
-        habilidades[3].classList.add("habilidadesdibujo");
-        habilidades[4].classList.add("adaptabilidad");
-        habilidades[5].classList.add("renderizado");
+        habilidades[0].classList.add("html");
+        habilidades[1].classList.add("css");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("javascript");
+        habilidades[4].classList.add("c");
+        habilidades[5].classList.add("sql");
         habilidades[6].classList.add("comunicacion");
         habilidades[7].classList.add("equipo");
         habilidades[8].classList.add("creatividad");
@@ -43,8 +43,8 @@ window.onscroll = function(){
 //Funcion descargar CV
 document.getElementById("descargarCV").addEventListener("click", function() {
     var enlaceDescarga = document.createElement('a');
-    enlaceDescarga.href = 'Document/prueba.pdf'; // Ruta al archivo PDF corregida
-    enlaceDescarga.download = 'prueba.pdf'; // Nombre del archivo PDF
+    enlaceDescarga.href = 'Document/CV Martin.pdf'; // Ruta al archivo PDF corregida
+    enlaceDescarga.download = 'CV Martin Aguilar.pdf'; // Nombre del archivo PDF
     document.body.appendChild(enlaceDescarga);
     enlaceDescarga.click();
     document.body.removeChild(enlaceDescarga);
